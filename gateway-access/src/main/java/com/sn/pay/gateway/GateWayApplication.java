7d7085982fd570f63c371c2d30f228fd0eb28b4d
zhangyu <zhangyu@ipaynow.cn>
2019-06-12 16:12:11 +0800

    修改日志文件

--- gateway-access/src/main/java/com/sn/pay/gateway/GateWayApplication.java
+++ gateway-access/src/main/java/com/sn/pay/gateway/GateWayApplication.java
@@ -1,7 +1,5 @@
 package com.sn.pay.gateway;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,12 +12,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class GateWayApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GateWayApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
 
-        LOGGER.info("应用启动完成");
     }
 
 }
