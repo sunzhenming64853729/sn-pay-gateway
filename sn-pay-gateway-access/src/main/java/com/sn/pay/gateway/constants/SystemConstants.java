403d4f54184d1bf7a9eb7c768856e1f9abcdb4d4
zhangyu <zhangyu@ipaynow.cn>
2019-07-04 11:52:35 +0800

    网关系统增加统一异常处理

---
+++ sn-pay-gateway-access/src/main/java/com/sn/pay/gateway/constants/SystemConstants.java
@@ -0,0 +1,20 @@
+package com.sn.pay.gateway.constants;
+
+/**
+ * @author ipaynow0531
+ * @date 2019/07/04
+ */
+public class SystemConstants {
+
+    public static final String RESPONSE_CODE = "responseCode";
+
+    public static final String CODE = "code";
+
+    public static final String FAIL = "FAIL";
+
+    public static final String EG001 = "EG001";
+
+    public static final String EA001_VALUE = "网关请求业务系统超时";
+
+    public static final String RESPONSE_MSG = "responseMsg";
+}
