0de1ccffb7fdbde514583f36f30ed576a3ebc958
fuboxiong <fuboxiong@sdiholding.com>
2019-08-20 13:01:42 +0800

    IOS测试环境--JVM参数优化

--- sn-pay-gateway-access/src/main/assembly/bin/start-test.sh
+++ sn-pay-gateway-access/src/main/assembly/bin/start-test.sh
@@ -75,7 +75,7 @@ fi
 JAVA_MEM_OPTS=""
 BITS=`java -version 2>&1 | grep -i 64-bit`
 if [ -n "$BITS" ]; then
-    JAVA_MEM_OPTS=" -server -Xmx2g -Xms2g -Xmn1024m -XX:PermSize=256m -XX:MaxPermSize=512m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
+    JAVA_MEM_OPTS=" -server -Xmx1g -Xms1g -Xmn512m -XX:PermSize=256m -XX:MaxPermSize=512m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
 else
     JAVA_MEM_OPTS=" -server -Xms1g -Xmx1g -XX:PermSize=256m -XX:SurvivorRatio=2 -XX:+UseParallelGC "
 fi
